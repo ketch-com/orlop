de0dc97a2aa8d1f99a815b4a38eb1a7b7957059a
Seth Yates <29878+sethyates@users.noreply.github.com>
2021-04-14 01:09:14 -0700

    feat: refactor to allow more control of Run

--- cmd.go
+++ cmd.go
@@ -25,7 +25,8 @@ import (
 	"fmt"
 	"github.com/iancoleman/strcase"
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/pflag"
+	"github.com/spf13/cobra"
+	"go.ketch.com/lib/orlop/errors"
 	"go.ketch.com/lib/orlop/log"
 	"go.opentelemetry.io/contrib/instrumentation/runtime"
 	"go.opentelemetry.io/otel/attribute"
@@ -37,62 +38,133 @@ import (
 	"time"
 )
 
-// Run loads config and then executes the given runner
-func Run(prefix string, runner interface{}, cfg interface{}) {
-	var configFiles []string
-	var initFlag bool
-	var envFlag string
-	var loglevelFlag string
-	pflag.BoolVar(&initFlag, "init", false, "outputs the config environment variables and exits")
-	pflag.StringVar(&envFlag, "env", strings.ToLower(getenv(prefix, "environment")), "specifies the environment")
-	pflag.StringVar(&loglevelFlag, "loglevel", strings.ToLower(getenv(prefix, "loglevel")), "specifies the log level")
-	pflag.StringSliceVar(&configFiles, "config", nil, "specifies a .env configuration file to load")
-	pflag.Parse()
+// Runner represents a command runner
+type Runner struct {
+	prefix      string
+	runner      interface{}
+	cfg         interface{}
+	prevPreRunE func(cmd *cobra.Command, args []string) error
+}
 
-	if initFlag {
-		vars, err := GetVariablesFromConfig(prefix, cfg)
-		if err != nil {
-			log.WithError(err).Fatal("could not create variables")
-		}
-
-		sort.Strings(vars)
-		for _, v := range vars {
-			if strings.Contains(v, "=#") {
-				fmt.Println("#" + v)
-			} else {
-				fmt.Println(v)
-			}
-		}
-		return
+// NewRunner creates a new Runner
+func NewRunner(prefix string, runner interface{}, cfg interface{}) *Runner {
+	return &Runner{
+		prefix: prefix,
+		runner: runner,
+		cfg:    cfg,
 	}
+}
+
+// SetupRoot sets up the root Command
+func (r *Runner) SetupRoot(cmd *cobra.Command) *Runner {
+	if cmd.PersistentFlags().Lookup("env") == nil {
+		cmd.PersistentFlags().String("env", strings.ToLower(r.Getenv("environment")), "specifies the environment")
+	}
+	if cmd.PersistentFlags().Lookup("loglevel") == nil {
+		cmd.PersistentFlags().String("loglevel", strings.ToLower(r.Getenv("loglevel")), "specifies the log level")
+	}
+	if cmd.PersistentFlags().Lookup("config") == nil {
+		cmd.PersistentFlags().StringSlice("config", nil, "specifies a .env configuration file to load")
+	}
+
+	r.prevPreRunE = cmd.PersistentPreRunE
+
+	if r.prevPreRunE == nil {
+		r.prevPreRunE = func(cmd *cobra.Command, args []string) error {
+			return nil
+		}
+	}
+
+	cmd.PersistentPreRunE = r.preRunE
+	return r
+}
+
+// Setup sets up the Command
+func (r *Runner) Setup(cmd *cobra.Command) *Runner {
+	cmd.RunE = r.runE
+
+	cmd.AddCommand(&cobra.Command{
+		Use:   "init",
+		Short: "output the config environment variables and exits",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			vars, err := GetVariablesFromConfig(r.prefix, r.cfg)
+			if err != nil {
+				log.WithError(err).Fatal("could not create variables")
+			}
+
+			sort.Strings(vars)
+
+			for _, v := range vars {
+				if strings.Contains(v, "=#") {
+					fmt.Println("#" + v)
+				} else {
+					fmt.Println(v)
+				}
+			}
+
+			return nil
+		},
+	})
+
+	return r
+}
+
+func (r *Runner) preRunE(cmd *cobra.Command, args []string) error {
+	envFlag, err := cmd.Flags().GetString("env")
+	if err != nil {
+		return err
+	}
+
+	loglevelFlag, err := cmd.Flags().GetString("loglevel")
+	if err != nil {
+		return err
+	}
+
+	configFiles, err := cmd.Flags().GetStringSlice("config")
+	if err != nil {
+		return err
+	}
+
+	// First figure out the environment
+	env := Environment(envFlag)
+
+	// Load the environment from files
+	loadEnvironment(env, configFiles...)
+
+	// Setup logging
+	r.SetupLogging(env, loglevelFlag)
 
 	if err := runtime.Start(runtime.WithMinimumReadMemStatsInterval(time.Second)); err != nil {
 		log.WithError(err).Fatal("could not start runtime tracing")
+	}
+
+	return r.prevPreRunE(cmd, args)
+}
+
+func (r *Runner) runE(cmd *cobra.Command, args []string) error {
+	envFlag, err := cmd.Flags().GetString("env")
+	if err != nil {
+		return err
 	}
 
 	ctx, span := tracer.Start(context.Background(), "Run")
 	defer span.End()
 
 	// First figure out the environment
-	env := Environment(envFlag)
-	span.SetAttributes(attribute.String("env", env.String()))
+	span.SetAttributes(attribute.String("env", Environment(envFlag).String()))
 
-	// Load the environment from files
-	loadEnvironment(env, configFiles...)
-
-	// Setup logging
-	ctx = setupLogging(ctx, env, loglevelFlag)
+	ctx = log.ToContext(ctx, log.New())
 
 	// Unmarshal the configuration
-	err := Unmarshal(prefix, cfg)
-	if err != nil {
+	if err = Unmarshal(r.prefix, r.cfg); err != nil {
 		log.FromContext(ctx).Fatal(err)
 	}
 
 	// Call the runner
-	out := reflect.ValueOf(runner).Call([]reflect.Value{
+	out := reflect.ValueOf(r.runner).Call([]reflect.Value{
 		reflect.ValueOf(ctx),
-		reflect.ValueOf(cfg),
+		reflect.ValueOf(r.cfg),
 	})
 
 	// Handle any result
@@ -100,16 +172,18 @@ func Run(prefix string, runner interface{}, cfg interface{}) {
 		e := out[0].MethodByName("Error")
 		out = e.Call([]reflect.Value{})
 		if len(out) > 0 && out[0].IsValid() {
-			log.FromContext(ctx).Fatal(out[0].String())
+			return errors.New(out[0].String())
 		}
 	}
+
+	return nil
 }
 
-func getenv(prefix string, key string) string {
-	return os.Getenv(strcase.ToScreamingSnake(strings.Join([]string{prefix, key}, "_")))
+func (r *Runner) Getenv(key string) string {
+	return os.Getenv(strcase.ToScreamingSnake(strings.Join([]string{r.prefix, key}, "_")))
 }
 
-func setupLogging(ctx context.Context, env Environment, loglevel string) context.Context {
+func (r *Runner) SetupLogging(env Environment, loglevel string) {
 	switch loglevel {
 	case "fatal":
 		logrus.SetLevel(logrus.FatalLevel)
@@ -144,6 +218,19 @@ func setupLogging(ctx context.Context, env Environment, loglevel string) context
 	}
 
 	stdlog.SetOutput(logrus.New().Writer())
+}
 
-	return log.ToContext(ctx, log.New())
+// Run loads config and then executes the given runner
+func Run(prefix string, runner interface{}, cfg interface{}) {
+	var cmd = &cobra.Command{
+		Use:              prefix,
+		TraverseChildren: true,
+		SilenceUsage:     true,
+	}
+
+	NewRunner(prefix, runner, cfg).SetupRoot(cmd).Setup(cmd)
+
+	if err := cmd.Execute(); err != nil {
+		log.WithError(err).Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/cobra v1.1.3 // indirect
 	github.com/spf13/pflag v1.0.5
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.19.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.19.0
