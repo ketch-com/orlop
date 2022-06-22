4f1e7e767b8675389cd5c793486f01d4000dcadf
Seth Yates <29878+sethyates@users.noreply.github.com>
2022-06-22 13:06:50 -0700

    fix: make more requests more extensible

--- request/context.go
+++ request/context.go
@@ -5,65 +5,58 @@ import (
 	"time"
 )
 
+type Key string
+
 var (
-	requestIDKey        = struct{}{}
-	requestURLKey       = struct{}{}
-	requestTimestampKey = struct{}{}
-	requestTenantKey    = struct{}{}
+	IDKey        Key = "request_id"
+	URLKey       Key = "request_url"
+	TimestampKey Key = "request_timestamp"
+	TenantKey    Key = "request_tenant"
 )
 
-func ID(ctx context.Context) string {
-	if v := ctx.Value(requestIDKey); v != nil {
-		if r, ok := v.(string); ok {
+func Value[T any](ctx context.Context, key Key) T {
+	if v := ctx.Value(key); v != nil {
+		if r, ok := v.(T); ok {
 			return r
 		}
 	}
 
-	return ""
+	var v T
+	return v
+}
+
+func WithValue[T any](parent context.Context, key Key, v T) context.Context {
+	return context.WithValue(parent, key, v)
+}
+
+func ID(ctx context.Context) string {
+	return Value[string](ctx, IDKey)
 }
 
 func WithID(parent context.Context, requestID string) context.Context {
-	return context.WithValue(parent, requestIDKey, requestID)
+	return WithValue(parent, IDKey, requestID)
 }
 
 func URL(ctx context.Context) string {
-	if v := ctx.Value(requestURLKey); v != nil {
-		if r, ok := v.(string); ok {
-			return r
-		}
-	}
-
-	return ""
+	return Value[string](ctx, URLKey)
 }
 
 func WithURL(parent context.Context, requestURL string) context.Context {
-	return context.WithValue(parent, requestURLKey, requestURL)
+	return WithValue(parent, URLKey, requestURL)
 }
 
 func Timestamp(ctx context.Context) time.Time {
-	if v := ctx.Value(requestTimestampKey); v != nil {
-		if r, ok := v.(time.Time); ok {
-			return r
-		}
-	}
-
-	return time.Time{}
+	return Value[time.Time](ctx, TimestampKey)
 }
 
 func WithTimestamp(parent context.Context, requestTimestamp time.Time) context.Context {
-	return context.WithValue(parent, requestTimestampKey, requestTimestamp)
+	return WithValue(parent, TimestampKey, requestTimestamp)
 }
 
 func Tenant(ctx context.Context) string {
-	if v := ctx.Value(requestTenantKey); v != nil {
-		if r, ok := v.(string); ok {
-			return r
-		}
-	}
-
-	return ""
+	return Value[string](ctx, TenantKey)
 }
 
 func WithTenant(parent context.Context, requestTenant string) context.Context {
-	return context.WithValue(parent, requestTenantKey, requestTenant)
+	return WithValue(parent, TenantKey, requestTenant)
 }
