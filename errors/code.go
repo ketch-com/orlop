babd9fee477fcaac8286e4246c5304d38af6598d
Seth Yates <29878+sethyates@users.noreply.github.com>
2022-06-22 13:32:51 -0700

    feat(KD-3337): implement a consistent error management strategy

---
+++ errors/code.go
@@ -0,0 +1,90 @@
+// Copyright (c) 2021 Ketch Kloud, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package errors
+
+import (
+	"fmt"
+)
+
+type coder struct {
+	error
+	code ErrorCode
+}
+
+func (sc coder) Unwrap() error {
+	return sc.error
+}
+
+func (sc coder) Error() string {
+	return fmt.Sprintf("[%d] %v", sc.code, sc.error)
+}
+
+func (sc coder) ErrorCode() ErrorCode {
+	return sc.code
+}
+
+// WithCode adds a Coder to err's error chain.
+// Unlike pkg/errors, WithCode will wrap nil error.
+func WithCode(err error, code ErrorCode) error {
+	if err == nil {
+		err = New(string(code))
+	}
+	return coder{err, code}
+}
+
+// Code returns the code associated with an error.
+// If no code is found, it returns EINTERNAL.
+// As a special case, it checks for Timeout() and Temporary() errors and returns
+// ETIMEOUT and EUNAVAILABLE
+// respectively.
+// If err is nil, it returns empty string
+func Code(err error) (code ErrorCode) {
+	if err == nil {
+		return ""
+	}
+
+	var sc interface {
+		error
+		ErrorCode() ErrorCode
+	}
+
+	if As(err, &sc) {
+		return sc.ErrorCode()
+	}
+
+	var timeouter interface {
+		error
+		Timeout() bool
+	}
+	if As(err, &timeouter) && timeouter.Timeout() {
+		return ETIMEOUT
+	}
+
+	var temper interface {
+		error
+		Temporary() bool
+	}
+	if As(err, &temper) && temper.Temporary() {
+		return EUNAVAILABLE
+	}
+
+	return EINTERNAL
+}
---
+++ errors/user_message.go
@@ -0,0 +1,85 @@
+// Copyright (c) 2021 Ketch Kloud, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package errors
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type messenger struct {
+	error
+	msg string
+}
+
+func (msgr messenger) Unwrap() error {
+	return msgr.error
+}
+
+func (msgr messenger) UserMessage() string {
+	return msgr.msg
+}
+
+func (msgr messenger) StatusCode() int {
+	if code := StatusCode(msgr.error); code != http.StatusInternalServerError {
+		return code
+	}
+
+	return http.StatusBadRequest
+}
+
+// WithUserMessage adds a UserMessenger to err's error chain.
+// If a status code has not previously been set,
+// a default status of Bad Request (400) is added.
+// Unlike pkg/errors, WithUserMessage will wrap nil error.
+func WithUserMessage(err error, msg string) error {
+	if err == nil {
+		err = New("UserMessage<" + msg + ">")
+	}
+	return messenger{err, msg}
+}
+
+// WithUserMessagef calls fmt.Sprintf before calling WithUserMessage.
+func WithUserMessagef(err error, format string, v ...any) error {
+	return WithUserMessage(err, fmt.Sprintf(format, v...))
+}
+
+// UserMessage returns the user message associated with an error.
+// If no message is found, it checks StatusCode and returns that message.
+// Because the default status is 500, the default message is
+// "Internal Server Error".
+// If err is nil, it returns "".
+func UserMessage(err error) string {
+	if err == nil {
+		return ""
+	}
+
+	var um interface {
+		error
+		UserMessage() string
+	}
+
+	if As(err, &um) {
+		return um.UserMessage()
+	}
+
+	return http.StatusText(StatusCode(err))
+}
