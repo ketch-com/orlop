21e9ae3956bb5f7c89030f7c43c6e1fcd2e920ab
Seth Yates <29878+sethyates@users.noreply.github.com>
2022-06-28 13:40:00 -0700

    fix: noop object store (#230)

--- parameter/noop.go
+++ parameter/noop.go
@@ -26,7 +26,7 @@ import (
 
 type noopStore struct{}
 
-func NewNoopStore() ObjectStore {
+func NewNoopStore() Store {
 	return &noopStore{}
 }
 
@@ -44,16 +44,4 @@ func (c noopStore) Write(ctx context.Context, p string, data map[string]interfac
 
 func (c noopStore) Delete(ctx context.Context, p string) error {
 	return nil
-}
-
-func (c noopStore) ReadObject(ctx context.Context, p string, out interface{}) error {
-	return nil
-}
-
-func (c noopStore) WriteObject(ctx context.Context, p string, in interface{}) error {
-	return nil
-}
-
-func (c noopStore) GetEnabled() bool {
-	return false
 }
