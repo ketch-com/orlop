c96e788d92de05e5de5818be04ada70368a19f4c
Seth Yates <29878+sethyates@users.noreply.github.com>
2022-07-01 12:15:58 -0700

    fix: env casing inconsistency

--- env/environ.go
+++ env/environ.go
@@ -21,7 +21,6 @@
 package env
 
 import (
-	"github.com/iancoleman/strcase"
 	"go.ketch.com/lib/orlop/v2/service"
 	"os"
 	"strings"
@@ -42,5 +41,55 @@ type environImpl struct {
 }
 
 func (e environImpl) Getenv(key string) string {
-	return os.Getenv(strcase.ToScreamingSnake(strings.Join([]string{string(e.prefix), key}, "_")))
+	return os.Getenv(toScreamingDelimited(strings.Join([]string{string(e.prefix), key}, "_"), '_', 0, true))
+}
+
+func toScreamingDelimited(s string, delimiter uint8, ignore uint8, screaming bool) string {
+	s = strings.TrimSpace(s)
+	n := strings.Builder{}
+	n.Grow(len(s) + 2) // nominal 2 bytes of extra space for inserted delimiters
+	for i, v := range []byte(s) {
+		vIsCap := v >= 'A' && v <= 'Z'
+		vIsLow := v >= 'a' && v <= 'z'
+		if vIsLow && screaming {
+			v += 'A'
+			v -= 'a'
+		} else if vIsCap && !screaming {
+			v += 'a'
+			v -= 'A'
+		}
+
+		// treat acronyms as words, eg for JSONData -> JSON is a whole word
+		if i+1 < len(s) {
+			next := s[i+1]
+			vIsNum := v >= '0' && v <= '9'
+			nextIsCap := next >= 'A' && next <= 'Z'
+			nextIsLow := next >= 'a' && next <= 'z'
+			nextIsNum := next >= '0' && next <= '9'
+			// add underscore if next letter case type is changed
+			if (vIsCap && nextIsLow) || (vIsLow && nextIsCap) || (vIsNum && (nextIsCap || nextIsLow)) {
+				if prevIgnore := ignore > 0 && i > 0 && s[i-1] == ignore; !prevIgnore {
+					if vIsCap && nextIsLow {
+						if prevIsCap := i > 0 && s[i-1] >= 'A' && s[i-1] <= 'Z'; prevIsCap {
+							n.WriteByte(delimiter)
+						}
+					}
+					n.WriteByte(v)
+					if vIsLow || vIsNum || nextIsNum {
+						n.WriteByte(delimiter)
+					}
+					continue
+				}
+			}
+		}
+
+		if (v == ' ' || v == '_' || v == '-') && uint8(v) != ignore {
+			// replace space/underscore/hyphen with delimiter
+			n.WriteByte(delimiter)
+		} else {
+			n.WriteByte(v)
+		}
+	}
+
+	return n.String()
 }
